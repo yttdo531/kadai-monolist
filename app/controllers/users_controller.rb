0c69711337b26045e424929fa97fae7ff6cda70d
yasuhisa <yt>
2018-01-25 11:30:24 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -23,4 +25,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
