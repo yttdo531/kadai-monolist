a48ad5f1453ee51c00f5e33fcc338b28dd775fae
yasuhisa <yt>
2018-01-27 08:12:01 +0000

    kadai-have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -15,6 +15,12 @@ class OwnershipsController < ApplicationController
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
     end
+    
+    # Have 関係として保存
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
+    end
 
     redirect_back(fallback_location: root_path)
   end
@@ -22,11 +28,18 @@ class OwnershipsController < ApplicationController
   def destroy
     @item = Item.find(params[:item_id])
 
+    #Want
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
     end
 
+    #Have
+    if params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
+    end
+    
     redirect_back(fallback_location: root_path)
   end
 end
